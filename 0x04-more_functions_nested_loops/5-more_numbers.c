2395bb408d45ef528a7bbfa162cea50faf3515c4
Sommy20 <chi5chisom20@gmail.com>
2022-03-18 07:49:49 -0700

    A functions that prints 10x the numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,22 +1,21 @@
 #include "main.h"
 
 /**
- * more_numbers - prints numbers between 0 to 14
- * 10 times.
- * Return: no return.
- */
+* more_numbers - prints 10 times the numbers, from 0 to 14.
+*/
 void more_numbers(void)
 {
-        int i, ch;
+	int i;
+	char c;
 
-        for (i = 0; i < 10; i++)
-        {
-
-                for (ch = 0; i < 15; i++)
-                {
-                        if (ch >= 10)
-                                _putchar((ch / 10) + 48);
-                        _putchar((ch % 10) + 48);
-                }
-        }
-    
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			if (c > 9)
+			_putchar((c / 10) + '0');
+			_putchar((c % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
